2a30673fad08766508806d8c1de723789ed0c52f
AyanaTuryskulova <157636404+AyanaTuryskulova@users.noreply.github.com>
2024-04-29 11:25:36 +0500

    Delete

--- lecture_13/shop/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from .views import categories_view, category_view
-
-urlpatterns = [
-    path('categories', categories_view),
-    path('categories/<int:pk>/', category_view)
-]
