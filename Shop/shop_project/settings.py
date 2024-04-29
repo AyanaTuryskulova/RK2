f2a01f3b423a0ad34b6dfa9ede9b9e1cd8cdaee9
AyanaTuryskulova <turyskulova@gmail.com>
2024-04-29 11:20:25 +0500

    My Django Girls app, first commit

--- Shop/shop_project/settings.py
+++ Shop/shop_project/settings.py
@@ -122,9 +122,9 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
-STATICFILES_DIRS = [
-    BASE_DIR / 'static'
-]
+# STATICFILES_DIRS = [
+#     BASE_DIR / 'static'
+# ]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
@@ -134,11 +134,11 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media/'
 
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = 'asuleymanmockexample@gmail.com'
-EMAIL_HOST_PASSWORD = 'frksgkoeeverhcuw'
-EMAIL_PORT = 587
-EMAIL_USE_TLS = True
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+# EMAIL_HOST = 'smtp.gmail.com'
+# EMAIL_HOST_USER = 'asuleymanmockexample@gmail.com'
+# EMAIL_HOST_PASSWORD = 'frksgkoeeverhcuw'
+# EMAIL_PORT = 587
+# EMAIL_USE_TLS = True
+# EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 
 
